9f502f43d223c67a2f7e3f43c8c89b8c255198e2
XiSHEN0220 <shenxiluc@gmail.com>
2017-03-24 12:11:46 +0100

    first commit

---
+++ Code/Hidden/plot.py
@@ -0,0 +1,57 @@
+import numpy as np
+import pylab as pl 
+
+def process_txt( txt_path ) : 
+	f = open(txt_path, 'r')
+	data = f.read()
+	data = data.split('\n')
+	data = data[1:-1]
+	 
+	 
+
+	epoch = []
+
+	train_loss = []
+
+	valid_loss = []
+
+
+	for i in range(len(data)):
+		 line = data[i].split('\t')[1:]
+		 epoch.append( int(line[0]))
+		 train_loss.append(float(line[1]))
+		 valid_loss.append(float(line[2]))
+
+	return train_loss, valid_loss
+
+txt_files = ['log_training_100.txt',
+			 'log_training_200.txt',
+			'log_training_300.txt',
+			'log_training_400.txt',
+			'log_training_500.txt']
+
+c = ['r', 'g', 'b', 'c', 'k']
+
+for i in range(len(txt_files)) : 
+	train_loss, valid_loss = process_txt( txt_files[i] )
+	l = txt_files[i].replace('log_training_','train : Hidden units = ').replace('.txt', '')
+	pl.plot(train_loss, color = c[i], lw = 3, label = l)
+	pl.plot(valid_loss, '--', color = c[i], lw = 3, label = l.replace('train', 'valid'))
+
+pl.xlabel('Nb Epochs')
+pl.ylabel('Reconstruction loss')
+pl.legend(loc = 'best')
+pl.grid('on')
+pl.title('Hidden units influence on trainning, Batch size = 100')
+pl.show()
+
+
+'''pl.plot(epoch, train_loss, '--r', lw = 3, label = 'Train loss, ||X - X\'||')
+pl.plot(epoch, valid_loss, '--b', lw = 3, label = 'Validation loss, ||X - X\'||')
+pl.xlabel('Nb Epochs')
+pl.ylabel('||X - X\'||')
+pl.legend(loc = 'best')
+pl.grid('on')
+pl.title('Hidden units = 100, Learning rate = 0.001, 1-CD, batchsize = 100, validation ratio = 0.2, Validation Error = 43.99160')
+pl.show()'''
+
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
+
 # Restricted-Boltzmann-Machine
+
+In this project, we plan to discover deeply both theoretical and implementation aspects of the advances in RBM training.
+
+We present an implementation of RBM totally in Python, and the results obtained. We also compare its performance with a deep learning library. (Theano)
+
+## Requirement 
+
+* Python2.7
+* Numpy
+
+## How to Run
+
+```
+$python RBM.py --help
+```
+
+## Training 
+
+see Hinton's paper ([A Practical Guide to Training Restricted Boltzmann Machines](https://www.cs.toronto.edu/~hinton/absps/guideTR.pdf))
+
+
